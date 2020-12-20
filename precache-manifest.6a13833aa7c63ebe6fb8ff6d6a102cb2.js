e95a5405c78656f98ac1368eeb5ec2f6d2703eb7
Apurv Gupta <apurv3351@gmail.com>
2020-12-20 15:00:57 +0530

    Updates

---
+++ precache-manifest.6a13833aa7c63ebe6fb8ff6d6a102cb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f259c9af8a2244ce3c0b192577777fda",
+    "url": "/FoodRecipe/index.html"
+  },
+  {
+    "revision": "98a1356f6d544676b9e1",
+    "url": "/FoodRecipe/static/css/main.1b2fc8bf.chunk.css"
+  },
+  {
+    "revision": "545f096040c0984faff0",
+    "url": "/FoodRecipe/static/js/2.7e0c2259.chunk.js"
+  },
+  {
+    "revision": "98a1356f6d544676b9e1",
+    "url": "/FoodRecipe/static/js/main.249fc3b1.chunk.js"
+  },
+  {
+    "revision": "5d6fb21915d26e5987d7",
+    "url": "/FoodRecipe/static/js/runtime-main.a19ccead.js"
+  }
+]);
